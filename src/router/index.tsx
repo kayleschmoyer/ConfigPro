cf09cf9423444a0139b51156b9f33c4691e3c253
kayleschmoyer <35810800+kayleschmoyer@users.noreply.github.com>
2025-10-15 20:40:18 -0400

    Route dashboard after login

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage';
+import { DashboardPage } from '../pages/DashboardPage';
 
 export const AppRouter = () => (
   <Routes>
     <Route path="/" element={<LoginPage />} />
+    <Route path="/dashboard" element={<DashboardPage />} />
     <Route path="*" element={<Navigate to="/" replace />} />
   </Routes>
 );
