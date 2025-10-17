c7d51a44b61f53c1dd764e9a8fd481a8711a42c2
kayleschmoyer <35810800+kayleschmoyer@users.noreply.github.com>
2025-10-16 20:06:42 -0400

    Add daycare touchscreen dashboard page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { DaycareDashboardPage } from '../pages/free-starter-templates/daycare/DaycareDashboardPage';
 import { LoginPage } from '../pages/free-starter-templates/configpro/LoginPage';
 import { PointOfSalePage } from '../pages/additional-paid-features/configpro/PointOfSalePage';
 
 export const AppRouter = () => (
   <Routes>
     <Route path="/" element={<LoginPage />} />
+    <Route path="/daycare" element={<DaycareDashboardPage />} />
     <Route path="/pos" element={<PointOfSalePage />} />
     <Route path="/dashboard" element={<Navigate to="/pos" replace />} />
     <Route path="*" element={<Navigate to="/" replace />} />
