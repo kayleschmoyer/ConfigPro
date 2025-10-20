dd451429f45abb9a693a42e374d80dce509b463f
kayleschmoyer <35810800+kayleschmoyer@users.noreply.github.com>
2025-10-19 22:01:26 -0400

    Fix duplicate re-exports in shared features index

--- src/pages/shared/features/index.ts
+++ src/pages/shared/features/index.ts
@@ -10,49 +10,28 @@ export {
 
 export * from '../../../shared/types/core';
 
-export { SharedNav } from './SharedNav';
 export { default as SharedNav } from './SharedNav';
 
-export { SharedFeaturesPage } from './SharedFeaturesPage';
 export { default as SharedFeaturesPage } from './SharedFeaturesPage';
-export { ErrorBoundaryPage } from './error.boundary';
 export { default as ErrorBoundaryPage } from './error.boundary';
 
-export { UsersAndRolesPage } from './UsersAndRolesPage';
 export { default as UsersAndRolesPage } from './UsersAndRolesPage';
-export { OrgAndLocationsPage } from './OrgAndLocationsPage';
 export { default as OrgAndLocationsPage } from './OrgAndLocationsPage';
-export { CatalogAttributesPage } from './CatalogAttributesPage';
 export { default as CatalogAttributesPage } from './CatalogAttributesPage';
-export { PricingRulesPage } from './PricingRulesPage';
 export { default as PricingRulesPage } from './PricingRulesPage';
-export { TaxRulesPage } from './TaxRulesPage';
 export { default as TaxRulesPage } from './TaxRulesPage';
-export { PaymentProvidersPage } from './PaymentProvidersPage';
 export { default as PaymentProvidersPage } from './PaymentProvidersPage';
-export { DocumentsAndBrandingPage } from './DocumentsAndBrandingPage';
 export { default as DocumentsAndBrandingPage } from './DocumentsAndBrandingPage';
-export { OrderWorkflowPage } from './OrderWorkflowPage';
 export { default as OrderWorkflowPage } from './OrderWorkflowPage';
-export { InventorySettingsPage } from './InventorySettingsPage';
 export { default as InventorySettingsPage } from './InventorySettingsPage';
-export { CustomerFieldsPage } from './CustomerFieldsPage';
 export { default as CustomerFieldsPage } from './CustomerFieldsPage';
-export { NotificationsPage } from './NotificationsPage';
 export { default as NotificationsPage } from './NotificationsPage';
-export { ReportingPage } from './ReportingPage';
 export { default as ReportingPage } from './ReportingPage';
-export { DataImportExportPage } from './DataImportExportPage';
 export { default as DataImportExportPage } from './DataImportExportPage';
-export { FeatureFlagsPage } from './FeatureFlagsPage';
 export { default as FeatureFlagsPage } from './FeatureFlagsPage';
-export { AuditLogPage } from './AuditLogPage';
 export { default as AuditLogPage } from './AuditLogPage';
-export { LocalizationPage } from './LocalizationPage';
 export { default as LocalizationPage } from './LocalizationPage';
-export { BrandingPage } from './BrandingPage';
 export { default as BrandingPage } from './BrandingPage';
-export { TimeIntelligenceHubPage } from './TimeIntelligenceHubPage';
 export { default as TimeIntelligenceHubPage } from './TimeIntelligenceHubPage';
 
 export * from './api.client';
